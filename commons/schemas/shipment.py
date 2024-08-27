89bc3e858b3803e441a399104a24868ae9bc53a0
Ujjwal Garg <dev@nouiq.com>
2024-08-27 18:24:50 +0530

    adding deployment

--- commons/schemas/shipment.py
+++ commons/schemas/shipment.py
@@ -2,8 +2,34 @@ from sqlalchemy import Column, Integer, String, DateTime, Text, Enum, ForeignKey
 from sqlalchemy.orm import relationship
 from commons.enums import ScrapeStatus
 from commons.database import Base
-# Import the related class
-from commons.schemas.container import ContainerAvailability
+
+
+class ContainerAvailability(Base):
+    __tablename__ = "container_status_table"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    shipment_id = Column(Integer, ForeignKey("shipments.shipment_id"))
+    date = Column(DateTime, nullable=True)
+    port = Column(String, nullable=False)
+    terminal = Column(String, nullable=False)
+    container_number = Column(String, nullable=False, unique=True)
+    available = Column(String, nullable=False)
+    usda_status = Column(String, nullable=True)
+    last_free_date = Column(String, nullable=True)
+    location = Column(String, nullable=True)
+    custom_release_status = Column(String, nullable=True)
+    carrier_release_status = Column(String, nullable=True)
+    demurrage_amount = Column(String, nullable=True)
+    yard_terminal_release_status = Column(String, nullable=True)
+    last_updated_availability = Column(
+        DateTime, nullable=False, default=func.current_timestamp())
+
+    # Use string reference here
+    shipment = relationship("Shipment", back_populates="containers")
+
+    def __repr__(self):
+        return (f"<ContainerAvailability(container_number='{self.container_number}', "
+                f"available='{self.available}', "
+                f"last_updated_availability='{self.last_updated_availability}')>")
 
 
 class Shipment(Base):
