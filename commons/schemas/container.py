096cc17e2bce1d5be92742c8c6063473d77ab77a
Ujjwal Garg <dev@nouiq.com>
2024-08-27 18:18:54 +0530

    adding deployment

--- commons/schemas/container.py
+++ commons/schemas/container.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from commons.database import Base
+from sqlalchemy.sql import func
 
 
 class ContainerAvailability(Base):
