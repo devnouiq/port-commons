569bddb9ad32fe446f7aabcea7f7892f7542e1c5
Ujjwal Garg <dev@nouiq.com>
2024-08-28 16:13:26 +0530

    adding deployment

--- commons/utils/date.py
+++ commons/utils/date.py
@@ -3,8 +3,10 @@ from zoneinfo import ZoneInfo
 
 
 def get_current_datetime_in_est():
-    """Get the current datetime in Eastern Standard Time (EST) without timezone info."""
+    """Get the current datetime in Eastern Standard Time (EST) without timezone info and without microseconds."""
     # Get the current time in EST
     current_time_est = datetime.now(ZoneInfo("America/New_York"))
-    # Return a naive datetime by stripping the timezone info
-    return current_time_est.replace(tzinfo=None)
+    # Remove timezone info and microseconds
+    naive_datetime_without_microseconds = current_time_est.replace(
+        tzinfo=None, microsecond=0)
+    return naive_datetime_without_microseconds
