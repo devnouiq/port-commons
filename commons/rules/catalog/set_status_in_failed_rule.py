f4b71b3dbe647a5555677c4b9082d96820151564
Ujjwal Garg <dev@nouiq.com>
2024-08-30 16:01:26 +0530

    Adding FastAPI code

--- commons/rules/catalog/set_status_in_failed_rule.py
+++ commons/rules/catalog/set_status_in_failed_rule.py
@@ -2,8 +2,6 @@ from commons.rules.engine import BusinessRule
 from typing import Dict, Any
 from commons.enums import ScrapeStatus
 from commons.utils.logger import get_logger
-from commons.utils.date import get_current_datetime_in_est
-from datetime import timedelta
 
 logger = get_logger()
 
@@ -18,10 +16,7 @@ class SetFailedStatusRule(BusinessRule):
             logger.info(
                 f"Setting status to FAILED and updating last_scraped_time for shipment ID {shipment.shipment_id}")
 
-            shipment.scrape_status = ScrapeStatus.ACTIVE
-            shipment.last_scraped_time = get_current_datetime_in_est() - timedelta(days=1)
-            shipment.next_scrape_time = shipment.last_scraped_time + \
-                timedelta(hours=shipment.frequency)
+            shipment.scrape_status = ScrapeStatus.FAILED
 
             logger.info(
                 f"Shipment ID {shipment.shipment_id} status set to FAILED and last_scraped_time updated to {shipment.last_scraped_time}")
