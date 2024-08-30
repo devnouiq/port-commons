c5aa181b5ed170f3c7fdc58212bcb700a92fe631
Ujjwal Garg <dev@nouiq.com>
2024-08-30 11:19:25 +0530

    adding deployment

--- commons/repository.py
+++ commons/repository.py
@@ -3,6 +3,10 @@ from sqlalchemy.exc import SQLAlchemyError
 from typing import Any, Dict, List
 from commons.enums import ScrapeStatus
 from commons.utils.date import get_current_datetime_in_est
+from commons.utils.logger import get_logger
+
+
+logger = get_logger()
 
 
 class BaseRepository:
@@ -12,6 +16,7 @@ class BaseRepository:
 
     def save(self, entity: Any):
         try:
+            logger.info(f"Saving entity: {entity}")
             self.session.add(entity)
             self.session.commit()  # Commit after save
         except SQLAlchemyError as e:
@@ -27,10 +32,13 @@ class BaseRepository:
             self.session.rollback()  # Rollback on error
             raise ValueError(f"Failed to bulk save entities: {str(e)}")
 
-    def update(self, entity: Any, updates: Dict[str, Any]):
+    def update(self, entity: Any, updates: Any):
         try:
-            for key, value in updates.items():
-                setattr(entity, key, value)
+            logger.info(f"Updating entity: {entity}")
+            # Iterate over the attributes of the updates object
+            for key, value in updates.__dict__.items():
+                if key != '_sa_instance_state':  # Skip internal SQLAlchemy state
+                    setattr(entity, key, value)
             self.session.commit()  # Commit after update
         except SQLAlchemyError as e:
             self.session.rollback()
@@ -50,12 +58,13 @@ class BaseRepository:
         except SQLAlchemyError as e:
             raise ValueError(f"Failed to get entity by ID: {str(e)}")
 
-    def get_by_container_number(self, container_number: str) -> Any:
+    def get_by_container_number_and_shipment_id(self, container_number: str, shipment_id: int) -> Any:
         try:
-            return self.session.query(self.model).filter_by(container_number=container_number).first()
+            return self.session.query(self.model).filter_by(
+                container_number=container_number, shipment_id=shipment_id).first()
         except SQLAlchemyError as e:
             raise ValueError(
-                f"Failed to get entity by container number: {str(e)}")
+                f"Failed to get entity by container number and shipment ID: {str(e)}")
 
     def _update_by_id(self, entity_id: int, updates: Dict[str, Any]):
         try:
@@ -80,10 +89,17 @@ class BaseRepository:
         }
         self._update_by_id(entity_id, updates)
 
-    def prepare_and_update_completed(self, entity_id: int):
+    def prepare_and_update_active(self, entity_id: int):
         updates = {
             'scrape_status': ScrapeStatus.ACTIVE,
             'next_scrape_time': get_current_datetime_in_est()
+        }
+        self._update_by_id(entity_id, updates)
+
+    def prepare_and_update_stopped(self, entity_id: int):
+        updates = {
+            'scrape_status': ScrapeStatus.STOPPED,
+            'next_scrape_time': None
         }
         self._update_by_id(entity_id, updates)
 
@@ -99,3 +115,21 @@ class BaseRepository:
         except SQLAlchemyError as e:
             self.session.rollback()
             raise ValueError(f"Failed to save or update entity: {str(e)}")
+
+    def handle_missing_container(self, container_number: str, shipment_id: int):
+        """
+        Handle the scenario where a container is not found in the scraped results.
+
+        :param container_number: The container number that was not found.
+        :param shipment_id: The shipment ID associated with the container.
+        """
+        try:
+            existing_record = self.get_by_container_number_and_shipment_id(
+                container_number, shipment_id)
+            if existing_record and existing_record.scrape_status == ScrapeStatus.ACTIVE:
+                self.prepare_and_update_stopped(shipment_id)
+            else:
+                self.prepare_and_update_failed(
+                    shipment_id, "Scraping failed: Container number not found in results.")
+        except SQLAlchemyError as e:
+            raise ValueError(f"Failed to handle missing container: {str(e)}")
