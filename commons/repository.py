d1973b9300ef4eb99daebde855851119b2075bce
Ujjwal Garg <dev@nouiq.com>
2024-08-30 18:07:20 +0530

    Adding FastAPI code

--- commons/repository.py
+++ commons/repository.py
@@ -1,8 +1,6 @@
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
 from typing import Any
-from commons.enums import ScrapeStatus
-from commons.utils.date import get_current_datetime_in_est
 from commons.utils.logger import get_logger
 
 logger = get_logger()
@@ -20,6 +18,7 @@ class BaseRepository:
             self.session.commit()  # Commit after save
         except SQLAlchemyError as e:
             self.session.rollback()
+            logger.error(f"Error saving entity: {str(e)}", exc_info=True)
             raise ValueError(f"Failed to save entity: {str(e)}")
 
     def update(self, entity: Any, updates: Any):
@@ -31,6 +30,7 @@ class BaseRepository:
             self.session.commit()  # Commit after update
         except SQLAlchemyError as e:
             self.session.rollback()
+            logger.error(f"Error updating entity: {str(e)}", exc_info=True)
             raise ValueError(f"Failed to update entity: {str(e)}")
 
     def save_or_update(self, entity: Any, unique_field: str, unique_value: Any):
@@ -43,36 +43,35 @@ class BaseRepository:
                 self.save(entity)
         except SQLAlchemyError as e:
             self.session.rollback()
+            logger.error(
+                f"Error saving or updating entity: {str(e)}", exc_info=True)
             raise ValueError(f"Failed to save or update entity: {str(e)}")
 
     def delete(self, entity: Any):
         try:
+            logger.info(f"Deleting entity: {entity}")
             self.session.delete(entity)
             self.session.commit()  # Commit after delete
         except SQLAlchemyError as e:
             self.session.rollback()
+            logger.error(f"Error deleting entity: {str(e)}", exc_info=True)
             raise ValueError(f"Failed to delete entity: {str(e)}")
 
     def get_by_id(self, entity_id: int) -> Any:
         try:
             return self.session.query(self.model).get(entity_id)
         except SQLAlchemyError as e:
+            logger.error(
+                f"Error fetching entity by ID: {str(e)}", exc_info=True)
+            self.session.rollback()
             raise ValueError(f"Failed to get entity by ID: {str(e)}")
 
-    def get_by_container_number_and_shipment_id(self, container_number: str, shipment_id: int) -> Any:
+    def get_by_fields(self, **kwargs) -> Any:
         try:
-            return self.session.query(self.model).filter_by(
-                container_number=container_number, shipment_id=shipment_id).first()
+            logger.info(f"Fetching entity by fields: {kwargs}")
+            return self.session.query(self.model).filter_by(**kwargs).first()
         except SQLAlchemyError as e:
-            raise ValueError(
-                f"Failed to get entity by container number and shipment ID: {str(e)}")
-
-    def prepare_and_update_status(self, entity_id: int, status: ScrapeStatus, error_message: str = None):
-        updates = self.model(
-            scrape_status=status,
-            last_scraped_time=get_current_datetime_in_est(),
-            next_scrape_time=get_current_datetime_in_est(
-            ) if status == ScrapeStatus.ACTIVE else None,
-            error=error_message if status == ScrapeStatus.FAILED else None
-        )
-        self.save_or_update(updates, 'shipment_id', entity_id)
+            logger.error(
+                f"Error fetching entity by fields: {str(e)}", exc_info=True)
+            self.session.rollback()
+            raise ValueError(f"Failed to get entity by fields: {str(e)}")
