3c00f0a08972729752706b940407678f8c036f4b
Ujjwal Garg <dev@nouiq.com>
2024-08-30 14:37:19 +0530

    adding deployment

--- commons/services/shipment.py
+++ commons/services/shipment.py
@@ -3,6 +3,7 @@ from commons.enums import ScrapeStatus
 from commons.utils.date import get_current_datetime_in_est
 from commons.utils.logger import get_logger
 from sqlalchemy.orm import Session
+from typing import Dict, Any, Optional
 
 logger = get_logger()
 
@@ -22,27 +23,31 @@ class ShipmentService:
         existing_container = context.get('container')
         container_availability = context.get('container_availability')
 
-        # Mark the shipment and container availability as ACTIVE
-        shipment.scrape_status = ScrapeStatus.ACTIVE
-        shipment.last_scraped_time = get_current_datetime_in_est()
+        # Mark the shipment and container availability as ACTIVE if they exist
+        if shipment:
+            shipment.scrape_status = ScrapeStatus.ACTIVE
+            shipment.last_scraped_time = get_current_datetime_in_est()
 
         # Call the process method to apply rules and save the data
         self.process(shipment, existing_container, container_availability)
 
-    def process(self, shipment, existing_container, container_availability):
+    def process(self, shipment: Optional[Any] = None, existing_container: Optional[Any] = None, container_availability: Optional[Any] = None):
         """
         Apply the business rules and save the updated shipment and container availability.
         """
         # Apply rules to shipment and container availability
+        context = {"shipment": shipment,
+                   "container_availability": container_availability}
         for rule in self.rules:
-            rule.apply(
-                {"shipment": shipment, "container_availability": container_availability})
+            rule.apply(context)
 
-        # Save or update the shipment
-        self.shipment_repo.save_or_update(
-            shipment, "shipment_id", shipment.shipment_id)
+        # Save or update the shipment if it exists
+        if shipment:
+            self.shipment_repo.save_or_update(
+                shipment, "shipment_id", shipment.shipment_id)
 
-        # Save or update the ContainerAvailability
-        container_availability.shipment_id = shipment.shipment_id
-        self.container_repo.save_or_update(
-            container_availability, "container_number", container_availability.container_number)
+        # Save or update the ContainerAvailability if it exists
+        if container_availability:
+            container_availability.shipment_id = shipment.shipment_id if shipment else container_availability.shipment_id
+            self.container_repo.save_or_update(
+                container_availability, "container_number", container_availability.container_number)
