c2032706988b0a69115f53ae4688bab2a9ebd0d9
Ujjwal Garg <dev@nouiq.com>
2024-09-28 05:49:40 +0530

    adding custom attributes

--- commons/proxy.py
+++ commons/proxy.py
@@ -1,4 +1,19 @@
-PROXIES = {
-    "http": "http://173.208.213.170:15002",
-    "https": "http://173.208.213.170:15002"
-}
+import random
+
+# List of proxies
+PROXY_LIST = [
+    "http://173.208.213.170:15002",
+    "http://173.208.150.242:15002",
+    "http://173.208.239.10:15002",
+    "http://173.208.136.2:15002"
+]
+
+
+def get_random_proxy():
+    """
+    Returns a random proxy from the PROXY_LIST.
+    """
+    return {
+        "http": random.choice(PROXY_LIST),
+        "https": random.choice(PROXY_LIST)
+    }
